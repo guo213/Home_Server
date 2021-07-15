90c6b1e57e4a10f1adc05ad77aefa2cf9921ed15
xu213 <1548689263@qq.com>
2021-07-15 11:58:38 +0800

    CRLF->LF

--- apps/admin/src/main.ts
+++ apps/admin/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AdminModule } from './admin.module';
 
+const PORT = 3001;
+
 async function bootstrap() {
   const app = await NestFactory.create(AdminModule);
-  await app.listen(3000);
+  await app.listen(PORT);
+  console.log(`lesten in http://localhost:${PORT}`);
 }
 bootstrap();
