d9de74fc9951ab0f33e49312162bcf0e768d8203
xu213 <1548689263@qq.com>
2021-11-14 12:08:55 +0800

    。

--- apps/admin/src/users/entities/user.entity.ts
+++ apps/admin/src/users/entities/user.entity.ts
@@ -1,5 +1,6 @@
 import { prop } from '@typegoose/typegoose';
 import { ApiProperty } from '@nestjs/swagger';
+import { Schema } from 'mongoose';
 
 export class User {
   @ApiProperty({ example: '123', description: '用户名' })
@@ -8,4 +9,7 @@ export class User {
   @ApiProperty({ example: '123', description: '密码' })
   @prop()
   password: string;
+  @ApiProperty({ example: '123', description: '仪表板列表' })
+  @prop()
+  dashboards?: [{ type: Schema.Types.ObjectId; ref: 'Dashboard' }];
 }
